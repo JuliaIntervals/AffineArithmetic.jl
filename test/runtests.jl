c17d2cc13951195c76a1a955d381ae75995b235f
Olivier Hénot <38465572+OlivierHnt@users.noreply.github.com>
2025-04-26 01:17:15 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,38 +4,38 @@ using Test
 using AffineArithmetic: Aff
 
 @testset "Construction from intervals" begin
-    X = 1..3
+    X = interval(1, 3)
     X_a = Affine(X)
     X_a isa Affine
 
-    Y = 2..4
+    Y = interval(2, 4)
     Y_a = Affine(Y)
-    @test Y_a.range == 2..4
-    @test Y_a.affine == Aff(3.0, SVector{1, Float64}(1.0), 0..0)
+    @test isequal_interval(Y_a.range, interval(2, 4))
+    @test Y_a.affine == Aff(3.0, SVector{1, Float64}(1.0), interval(0))
 
     sum_a = X_a + Y_a
-    @test sum_a.range == 3..7
-    @test sum_a.affine == Aff(5.0, SVector{1, Float64}(2.0), 0..0)
+    @test isequal_interval(sum_a.range, interval(3, 7))
+    @test sum_a.affine == Aff(5.0, SVector{1, Float64}(2.0), interval(0))
 
     prod_a = X_a * Y_a
-    @test prod_a.range == 2..12
-    @test prod_a.affine == Aff(6.0, SVector{1, Float64}(5.0), 0..1)
+    @test isequal_interval(prod_a.range, interval(2, 12))
+    @test prod_a.affine == Aff(6.0, SVector{1, Float64}(5.0), interval(0, 1))
 end
 
 @testset "Small powers and range" begin
-    X = 1..3
+    X = interval(1, 3)
     Y = Affine(X)
 
     f(x) = x^2 - x + 1
 
-    @test range(f(X)) == -1..9
-    @test range(f(Y)) == 0..7   # affine is a little better
+    @test isequal_interval(range(f(X)), interval(-1, 9))
+    @test isequal_interval(range(f(Y)), interval(0, 7))   # affine is a little better
 
 
     g(x) = (x - 1)^3
 
-    @test range(g(X)) == 0..8
-    @test range(g(Y)) == 0..8   # affine gives the same
+    @test isequal_interval(range(g(X)), interval(0, 8))
+    @test isequal_interval(range(g(Y)), interval(0, 8))   # affine gives the same
 end
 
 reset_affine_index()
@@ -43,15 +43,15 @@ reset_affine_index()
 @testset "Matrix multiplication" begin
     A = 0.5 * [1 2; -1 1]
 
-    X = Affine(-1..1)
-    Y = Affine(-1..1)
+    X = Affine(interval(-1, 1))
+    Y = Affine(interval(-1, 1))
 
     XX = [X, Y]
-    @test XX == [Affine(Aff(0.0, SVector{1, Float64}(1.0), 0..0), -1..1),
-                 Affine(Aff(0.0, SVector{1, Float64}(1.0), 0..0), -1..1)]
+    @test all(isequal_interval.(XX, [Affine(Aff(0.0, SVector{1, Float64}(1.0), interval(0)), interval(-1, 1)),
+                                     Affine(Aff(0.0, SVector{1, Float64}(1.0), interval(0)), interval(-1, 1))]))
 
-    @test A * XX == [Affine(Aff(0.0, SVector{1, Float64}(1.5), 0..0), -1.5 .. 1.5),
-                     Affine(Aff(0.0, SVector{1, Float64}(0.0), 0..0), 0..0)]
+    @test all(isequal_interval.(A * XX, [Affine(Aff(0.0, SVector{1, Float64}(1.5), interval(0)), interval(-1.5, 1.5)),
+                                         Affine(Aff(0.0, SVector{1, Float64}(0.0), interval(0)), interval(0))]))
 end
 
 reset_affine_index()
@@ -61,18 +61,18 @@ reset_affine_index()
     p = Polynomial([-3, 1])   # x - 3
     p2 = p^8
 
-    x = 4 ± 1e-4
+    x = interval(4, 1e-4; format = :midpoint)
     y = Affine(x)
 
-    @test (-70..70) ⊆ range(p2(x))
-    @test range(p2(y)) ⊆ 0.998..1.002   # affine is extremely much better!
+    @test issubset_interval(interval(-70, 70), range(p2(x)))
+    @test issubset_interval(range(p2(y)), interval(0.998, 1.002))   # affine is extremely much better!
 end
 
 @testset "Constructors with intervals" begin
-    X = Affine(1..2)
+    X = Affine(interval(1, 2))
     @test X isa Affine
-    @test X - X == Affine(0..0)
+    @test X - X == Affine(interval(0))
 
-    x, y = affine(1..2, 3..4)
-    @test interval(x + y - x - y) == 0..0
+    x, y = affine(interval(1, 2), interval(3, 4))
+    @test isequal_interval(interval(x + y - x - y), interval(0))
 end
